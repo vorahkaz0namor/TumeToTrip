86a6d8298e7ab145fdbbacdf7d910f066fd1a7b4
Roman Zakharov <ambeadexter@yandex.ru>
2023-09-14 14:39:07 +0700

    Initial commit

---
+++ app/src/main/java/ru/sign/conditional/timetotrip/activity/AppActivity.kt
@@ -0,0 +1,11 @@
+package ru.sign.conditional.timetotrip.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.sign.conditional.timetotrip.R
+
+class AppActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
